4f983adc9097a94d6e5c85f928344b6211f3bf1a
skylerto <skylerclayne@gmail.com>
2015-09-11 09:06:01 -0400

    fixed package

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 var envdir;
+set('envdir', 'env');
 
-// check if the env folder is set.
+/*
+* Check if the env folder is set.
+*/
 if(envdir === undefined){
   console.log("**push-env: Environment directory not set!");
 } else {
   // check what the NODE_ENV env variable is, if it isn't set then use development
+  console.log(envdir);
 }
 
 
 
 var env = process.env.NODE_ENV;
 
+/*
+* Checking env
+*/
 if(env === undefined) {
   env = 'development';
   console.log("**push-env: NODE_ENV not set, using development");
+} else {
+
+}
+
+function set(key, value){
+  switch (key) {
+    case 'envdir':
+      envdir = './' + value;
+      break;
+    default:
+      // envdir = '.';
+  }
 }
